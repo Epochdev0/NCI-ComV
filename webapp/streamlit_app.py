fdc20ea6a9ce2cf0bec182001c0c3946acc58b96
temuulen-munkhtaivan <x23143509@student.ncirl.ie>
2025-10-04 11:54:02 +0100

    commit 1.0

--- webapp/streamlit_app.py
+++ webapp/streamlit_app.py
@@ -8,16 +8,18 @@ st.title('Exoplanet Classifier - Demo')
 model_path = st.sidebar.text_input('Model path', 'models/baseline.pkl')
 
 if st.sidebar.button('Load model'):
-clf = joblib.load(model_path)
-st.sidebar.success('Model loaded')
+	clf = joblib.load(model_path)
+	st.sidebar.success('Model loaded')
 
 uploaded = st.file_uploader('Upload CSV with features', type='csv')
+
 if uploaded:
-df = pd.read_csv(uploaded)
-st.write('Preview')
-st.dataframe(df.head())
+    df = pd.read_csv(uploaded)
+    st.write('Preview')
+    st.dataframe(df.head())
+    
 if st.button('Predict'):
-X = df
-preds = clf.predict(X)
-df['prediction'] = preds
-st.write(df[['prediction']].head())
+    X = df
+    preds = clf.predict(X)
+    df['prediction'] = preds
+    st.write(df[['prediction']].head())
